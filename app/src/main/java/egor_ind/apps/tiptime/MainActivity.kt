02f1d7c12442e66e8d0a1dc2e724713987ac1049
EGOR-IND <56648862+EGOR-IND@users.noreply.github.com>
2021-05-17 02:00:36 +0530

    add null checks and functionality to calculate button

--- app/src/main/java/egor_ind/apps/tiptime/MainActivity.kt
+++ app/src/main/java/egor_ind/apps/tiptime/MainActivity.kt
@@ -3,14 +3,41 @@ package egor_ind.apps.tiptime
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import egor_ind.apps.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.tipCalculateBtn.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val costOfService =
+            binding.servCostEt.text.toString().toDoubleOrNull()
+        if (costOfService == null) {
+            binding.tipAmountTxt.text = ""
+            binding.servCostEtOutbox.error = "Required!"
+            return
+        }
+        val tipPercent = when (binding.tipPercentRg.checkedRadioButtonId) {
+            R.id.twenty_per_tip -> 0.20
+            R.id.eighteen_per_tip -> 0.18
+            else -> 0.15
+        }
+
+        var tipAmount = costOfService*tipPercent
+        if (binding.tipRoundUpSwitch.isChecked) {
+            tipAmount = ceil(tipAmount)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tipAmount)
+        binding.tipAmountTxt.text = getString(R.string.tip_amount, formattedTip)
     }
 }
